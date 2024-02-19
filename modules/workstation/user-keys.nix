3810a7f52e9bea0f1b21a0837a577b97c3191c5b
Zebreus <lennarteichhorn@googlemail.com>
2024-02-19 15:38:01 +0100

    Fix user key permissions

--- modules/workstation/user-keys.nix
+++ modules/workstation/user-keys.nix
@@ -3,6 +3,8 @@
   age.identityPaths = [ config.age.secrets.lennart_ed25519.path "/home/lennart/.ssh/lennart_ed25519" ];
   age.secrets.lennart_ed25519 = {
     file = ../../secrets + "/lennart_ed25519.age";
+    owner = "lennart";
+    group = config.users.users.lennart.group;
     mode = "0400";
     path = "/home/lennart/.ssh/id_ed25519";
     # Copy the key, because it is used to decrypt the other keys
