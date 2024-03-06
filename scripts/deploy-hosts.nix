08a19cbc7dc2474d7d0b69ba5a7092175b8a8f59
Zebreus <lennarteichhorn@googlemail.com>
2024-03-06 15:32:17 +0100

    Wait for hosts to come online before deploying

--- scripts/deploy-hosts.nix
+++ scripts/deploy-hosts.nix
@@ -15,6 +15,10 @@ with pkgs; writeScriptBin "deploy-hosts" ''
   nixos-rebuild --flake .#kappril --target-host kappril test
   nixos-rebuild --flake .#kashenblade --target-host kashenblade test
 
+  until nc -vz -w 2 erms 22; do sleep 1; done 
+  until nc -vz -w 2 kashenblade 22; do sleep 1; done 
+  until nc -vz -w 2 kappril 22; do sleep 1; done 
+
   echo Making configurations boot default configurations...
   nixos-rebuild --flake .#erms --target-host erms boot
   nixos-rebuild --flake .#kappril --target-host kappril boot
