223defba19be810febb134d9129efad1460f9767
Zebreus <lennarteichhorn@googlemail.com>
2024-03-12 16:53:53 +0100

    Improve DNS zones

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   publicKeys = import ../secrets/public-keys.nix;
 
@@ -17,6 +17,7 @@ let
     in
     builtins.concatStringsSep " " (builtins.map (s: "\"" + s + "\"") chunks);
 
+  machinesThatCanReceiveMail = (lib.attrValues (lib.filterAttrs (name: machine: machine.managed) config.machines));
 in
 {
   networking.firewall.allowedTCPPorts = [ 53 ];
@@ -33,40 +34,34 @@ in
         $TTL 60
         $ORIGIN antibuild.ing.
         @ IN SOA ns1.antibuild.ing. lennart.zebre.us. (
-                1710252091  ; serial secs since Jan 1 1970
-                7200        ; refresh (>=60)
+                1710252093  ; serial secs since Jan 1 1970
+                14400        ; refresh (>=60)
                 3600        ; retry (>=60)
                 604800      ; expire
-                60          ; minimum ttl
+                300          ; minimum ttl
                 )
         @ IN NS ns1.antibuild.ing.
         @ IN NS ns2.antibuild.ing.
         ns1 IN A 167.235.154.30
         ns2 IN A 192.227.228.220
         ; ns1 IN AAAA 2a01:4f8:c0c:d91f::1
-
-        ; Records for mail
-        mail IN CNAME mail.zebre.us.
-        _dmarc.erms IN TXT ${quoteTxtEntry "v=DMARC1; p=none"}
-        _dmarc.kappril IN TXT ${quoteTxtEntry "v=DMARC1; p=none"}
-        _dmarc.kashenblade IN TXT ${quoteTxtEntry "v=DMARC1; p=none"}
-        _dmarc.sempriaq IN TXT ${quoteTxtEntry "v=DMARC1; p=none"}
-        erms IN TXT ${quoteTxtEntry "v=spf1 a:mail.zebre.us ip6:fd10:2030::0/64 mx a ptr -all"}
-        kappril IN TXT ${quoteTxtEntry "v=spf1 a:mail.zebre.us ip6:fd10:2030::0/64 mx a ptr -all"}
-        kashenblade IN TXT ${quoteTxtEntry "v=spf1 a:mail.zebre.us ip6:fd10:2030::0/64 mx a ptr -all"}
-        sempriaq IN TXT ${quoteTxtEntry "v=spf1 a:mail.zebre.us ip6:fd10:2030::0/64 mx a ptr -all"}
-        mail._domainkey.erms IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys.erms_dkim}"}
-        mail._domainkey.kappril IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys.kappril_dkim}"}
-        mail._domainkey.kashenblade IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys.kashenblade_dkim}"}
-        mail._domainkey.sempriaq IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys.sempriaq_dkim}"}
-
-
-        * IN MX 50 mail.zebre.us.
-        erms IN MX 30 mail.zebre.us.
-        kappril IN MX 30 mail.zebre.us.
-        kashenblade IN MX 30 mail.zebre.us.
-        sempriaq IN MX 30 mail.zebre.us.
-      '';
+        
+      '' + (builtins.concatStringsSep "\n" (
+        builtins.map
+          # TODO: Verify that autodiscovery works like this
+          (machine: ''
+            ; Mail records for ${machine.name}
+            _dmarc.${machine.name} IN TXT ${quoteTxtEntry "v=DMARC1; p=none"}
+            ${machine.name} IN TXT ${quoteTxtEntry "v=spf1 +ip6:${config.antibuilding.ipv6Prefix}::0/64 +a:mail.zebre.us +mx +a -all"}
+            mail._domainkey.${machine.name} IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys."${machine.name}_dkim"}"}
+            ${machine.name} IN MX 30 mail.zebre.us.
+            ; Autodiscovery
+            _submission._tcp.${machine.name} IN SRV 0 5 587 mail.zebreu.us.
+            _submissions._tcp.${machine.name} IN SRV 0 5 465 mail.zebreu.us.
+            _imap._tcp.${machine.name} IN SRV 0 5 143 mail.zebreu.us.
+            _imaps._tcp.${machine.name} IN SRV 0 5 993 mail.zebreu.us.
+          '')
+          machinesThatCanReceiveMail));
     };
 
     zones."zebre.us" = {
@@ -77,11 +72,11 @@ in
         $TTL 60
         $ORIGIN zebre.us.
         @ IN SOA ns1.antibuild.ing. lennart.zebre.us. (
-                1710252091  ; serial secs since Jan 1 1970
-                7200        ; refresh (>=60)
+                1710252093  ; serial secs since Jan 1 1970
+                14400        ; refresh (>=60)
                 3600        ; retry (>=60)
                 604800      ; expire
-                60          ; minimum ttl
+                300          ; minimum ttl
                 )
         @ IN NS ns1.antibuild.ing.
         @ IN NS ns2.antibuild.ing.
@@ -104,10 +99,10 @@ in
         mail._domainkey IN TXT ${quoteTxtEntry "v=DKIM1; k=rsa; s=email; p=${publicKeys.zebre_us_dkim}"}
         @ IN MX 30 mail.zebre.us.
         ; Autodiscovery:
-        _submission._tcp.zebre.us. 86400 IN SRV 0 5 587 mail.zebreu.us.
-        _submissions._tcp.zebre.us. 86400 IN SRV 0 5 465 mail.zebreu.us.
-        _imap._tcp.zebre.us. 86400 IN SRV 0 5 143 mail.zebreu.us.
-        _imaps._tcp.zebre.us. 86400 IN SRV 0 5 993 mail.zebreu.us.
+        _submission._tcp IN SRV 0 5 587 mail.zebreu.us.
+        _submissions._tcp IN SRV 0 5 465 mail.zebreu.us.
+        _imap._tcp IN SRV 0 5 143 mail.zebreu.us.
+        _imaps._tcp IN SRV 0 5 993 mail.zebreu.us.
       '';
     };
 
