9d420477c86203a179784b4fa9a32b8b0c239f63
Zebreus <lennarteichhorn@googlemail.com>
2024-03-12 03:49:02 +0100

    Fail at fixing DNS

--- modules/dns.nix
+++ modules/dns.nix
@@ -42,8 +42,8 @@ in
         IN  NS ns1.antibuild.ing.
         IN  NS ns2.antibuild.ing.
         ns1 IN	A	167.235.154.30
-        ns1 IN	AAAA	2a01:4f8:c0c:d91f::1
         ns2 IN	A	192.227.228.220
+        ; ns1 IN	AAAA	2a01:4f8:c0c:d91f::1
 
         ; Records for mail
         mail IN CNAME mail.zebre.us.
