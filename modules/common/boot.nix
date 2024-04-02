30a5f04e14518fbfce3c60bb051f110ad8194b1c
Zebreus <lennart@zebre.us>
2024-04-02 02:55:32 +0200

    Refactor boot.nix

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,22 +1,18 @@
 { pkgs, config, lib, ... }:
 {
-  options.modules.boot = {
-    type = lib.mkOption {
-      description = ''
-        How to configure the boot loader. The default is "efi" which installs systemd-boot into `/boot/efi`.
+  options.modules.boot.type = lib.mkOption {
+    description = ''
+      How to configure the boot loader. The default is "efi" which installs systemd-boot into `/boot/efi`.
 
-        "legacy" uses grub for BIOS systems. "raspi" uses extlinux for Raspberry Pi.
-      '';
-      type = lib.types.enum [ "efi" "legacy" "raspi" ];
-      default = "efi";
-    };
+      "legacy" uses grub for BIOS systems. "raspi" uses extlinux for Raspberry Pi.
+    '';
+    type = lib.types.enum [ "efi" "legacy" "raspi" ];
+    default = "efi";
   };
 
 
   config = {
     boot = {
-      # tmp.cleanOnBoot = true;
-
       kernelPackages = pkgs.linuxPackages_latest;
 
       loader = {
