2209fcbcb1d45887fb8ed83973b21db9af9382c1
Zebreus <lennart@zebre.us>
2024-04-07 15:11:19 +0200

    Add script for fixing home permissions

--- modules/workstation/lennart.nix
+++ modules/workstation/lennart.nix
@@ -19,7 +19,24 @@ in
         ];
         home = "/home/lennart";
         hashedPasswordFile = config.age.secrets.lennart_login_passwordhash.path;
+        createHome = true;
       };
+    };
+
+    # I have some weird permission errors in the user home directory with nixos-anywhere and agenix.
+    # I am to lazy to think of a proper solution, so this should work for now.
+    system.activationScripts = {
+      "fix-permissions" = ''
+        mkdir -p /home/lennart
+        chmod 700 /home/lennart
+        mkdir -p /home/lennart/.ssh /home/lennart/.cache /home/lennart/.config
+        chmod 700 /home/lennart/.ssh /home/lennart/.config
+        chmod 755 /home/lennart/.cache
+        # Recursively chown, if they are not owned by lennart
+        test "$(stat -c "%U" /home/lennart/.ssh)" == "root" && chown -R lennart:${config.users.users.lennart.group} /home/lennart/.ssh
+        test "$(stat -c "%U" /home/lennart/.config)" == "root" && chown -R lennart:${config.users.users.lennart.group} /home/lennart/.config
+        test "$(stat -c "%U" /home/lennart/.cache)" == "root" && chown -R lennart:${config.users.users.lennart.group} /home/lennart/.cache
+      '';
     };
   };
 }
