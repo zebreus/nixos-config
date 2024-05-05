e36b4ac448a29f1c7f23e4b04c8c916e71590c22
Izzie van de Haaien <izzie@haaien.xyz>
2024-05-05 11:24:21 +0200

    Refactor path to secret file

--- modules/workstation/homed.nix
+++ modules/workstation/homed.nix
@@ -3,7 +3,7 @@
     services.homed.enable = true;
 
     age.secrets."497a_homed" = {
-      file = ../../secrets + "/497a_homed.age";
+      file = ../../secrets/497a_homed.age;
       path = "/var/lib/systemd/home/497a.public";
     };
   };
