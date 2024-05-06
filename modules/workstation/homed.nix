74629cb4607525f556a52a3ce3d7d719863758a1
Zebreus <lennart@zebre.us>
2024-05-06 17:11:21 +0200

    Fix config

--- modules/workstation/homed.nix
+++ modules/workstation/homed.nix
@@ -1,10 +1,10 @@
 { lib, config, ... }: {
   config = lib.mkIf config.modules.workstation.enable {
     services.homed.enable = true;
-  };
 
-  age.secrets."497a_homed" = {
-        file = ../../secrets + "/497a_homed.age";
-        path = "/var/lib/systemd/home/497a.public";
+    age.secrets."497a_homed" = {
+      file = ../../secrets + "/497a_homed.age";
+      path = "/var/lib/systemd/home/497a.public";
+    };
   };
 }
