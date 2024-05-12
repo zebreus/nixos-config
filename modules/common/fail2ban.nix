defdbecbb3692bf4bd3b84e771686474a5868144
Zebreus <lennart@zebre.us>
2024-05-12 19:52:54 +0200

    Increase fail2ban limits

--- modules/common/fail2ban.nix
+++ modules/common/fail2ban.nix
@@ -1,13 +1,15 @@
 {
   services.fail2ban = {
     enable = true;
-    maxretry = 5;
+    maxretry = 10;
     ignoreIP = [
       # Do not ban traffic from the antibuilding
       # TODO: Maybe only not ban traffic from workstations?
       "fd10:2030::0/64"
+      # Always allow from traffic from this local network
+      "192.168.2.0/24"
     ];
-    bantime = "10m";
+    bantime = "5m";
     bantime-increment = {
       enable = false;
       maxtime = "48h";
