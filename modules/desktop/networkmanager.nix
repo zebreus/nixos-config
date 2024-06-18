3d936df8985ec29187756ae84e93769a82cd8ddc
Zebreus <zebreus@zebre.us>
2024-06-18 14:11:57 +0200

    Fix problem with network-manager wait online

--- modules/desktop/networkmanager.nix
+++ modules/desktop/networkmanager.nix
@@ -1,7 +1,13 @@
 # Enable networkmanager
-{ lib, config, ... }: {
+{ lib, config, pkgs, ... }: {
   config = lib.mkIf config.machines.${config.networking.hostName}.desktop.enable {
     networking.networkmanager.enable = true;
+    # Wait online creates more problems than its worth
+    systemd.services.NetworkManager-wait-online = {
+      serviceConfig = {
+        ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
+      };
+    };
 
     users.extraGroups.networkmanager.members = [ "lennart" ];
   };
