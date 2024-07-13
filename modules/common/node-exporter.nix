64e2097ae4935fd4ede53b1cc1fe2be660a06692
Zebreus <zebreus@zebre.us>
2024-07-13 16:10:17 +0200

    Fix node exporter firewall rules

--- modules/common/node-exporter.nix
+++ modules/common/node-exporter.nix
@@ -6,17 +6,18 @@ let
 in
 {
   config = {
+    # Open firewall port 9100 for traffic from the grafana server
+    networking.firewall.extraInputRules = lib.mkMerge (builtins.map
+      (machine: ''
+        ip6 saddr { ${config.antibuilding.ipv6Prefix}::${builtins.toString machine.address}/128 } tcp dport ${builtins.toString config.services.prometheus.exporters.node.port} accept
+      '')
+      grafanaServers);
     services.prometheus = {
       exporters.node = {
         enable = true;
         enabledCollectors = [ "systemd" ];
         listenAddress = "[${config.antibuilding.ipv6Prefix}::${builtins.toString thisMachine.address}]";
         port = 9100;
-        openFirewall = true;
-        firewallFilter =
-          if ((builtins.length grafanaServers) > 0) then "-p tcp -m tcp --dport 9100 -s ${lib.concatMapStringsSep "," 
-        (machine: "${config.antibuilding.ipv6Prefix}::${builtins.toString machine.address}/128")
-        grafanaServers}" else null;
       };
     };
   };
