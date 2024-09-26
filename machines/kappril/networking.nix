ad4b7e86d1cb8f2c0793f53adc3256f93935afac
Zebreus <zebreus@zebre.us>
2024-09-26 15:26:36 +0200

    Try to fix kappril

--- machines/kappril/networking.nix
+++ machines/kappril/networking.nix
@@ -2,8 +2,9 @@
 {
   networking = {
     wireless.enable = false;
-    useDHCP = false;
+    # useDHCP = false;
     networkmanager.enable = true;
   };
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.network.wait-online.enable = false;
 }
