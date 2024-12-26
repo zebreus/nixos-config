f3fae6e638dc94f0bf7c41a6f710f45c11dd027f
Zebreus <zebreus@zebre.us>
2024-12-26 11:32:09 +0100

    Remove voids root backdoor

--- modules/workstation/homed.nix
+++ modules/workstation/homed.nix
@@ -2,9 +2,10 @@
   config = lib.mkIf config.machines.${config.networking.hostName}.workstation.enable {
     services.homed.enable = true;
 
-    age.secrets."497a_homed" = {
-      file = ../../secrets/497a_homed.age;
-      path = "/var/lib/systemd/home/497a.public";
-    };
+    # # This gives 497a root on my machine
+    # age.secrets."497a_homed" = {
+    #   file = ../../secrets/497a_homed.age;
+    #   path = "/var/lib/systemd/home/497a.public";
+    # };
   };
 }
