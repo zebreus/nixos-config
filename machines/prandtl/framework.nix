f51e75d15289c8ce99f360fddd9672066826c41f
Zebreus <zebreus@zebre.us>
2025-01-05 04:30:46 +0100

    Comment the framework kernel module

--- machines/prandtl/framework.nix
+++ machines/prandtl/framework.nix
@@ -6,7 +6,7 @@
 {
 
   # Enable the community created Framework kernel module that allows interacting with the embedded controller from sysfs.
-  boot.extraModulePackages = with config.boot.kernelPackages; [ framework-laptop-kmod ];
+  # boot.extraModulePackages = with config.boot.kernelPackages; [ framework-laptop-kmod ];
   # https://github.com/DHowett/framework-laptop-kmod?tab=readme-ov-file#usage
   boot.kernelModules = [
     "cros_ec"
