3e6ad3112dec35c8652e4f3092ea39f954d20d37
Zebreus <zebreus@zebre.us>
2025-01-18 18:23:14 +0100

    Improve peering script

--- scripts/add-dn42-peer.argbash.sh
+++ scripts/add-dn42-peer.argbash.sh
@@ -11,7 +11,6 @@
 # [ <-- needed because of Argbash
 
 PUBLIC_KEY=""
-set -x
 
 function generateKeys {
     PREVIOUS_DIR=$(pwd)
@@ -55,16 +54,24 @@ function generateKeys {
 function generateConfig {
     sed -i 's/dn42Peerings = \[/dn42Peerings = \[ "'"$PEERING_NAME"'"/' machines.nix # ]] <-- needed because of Argbash
 
+    THEIR_WIREGUARD_ENDPOINT_LINE=""
+    if [ -n "$THEIR_WIREGUARD_ENDPOINT" ]; then
+        THEIR_WIREGUARD_ENDPOINT_LINE="publicWireguardEndpoint = "'"'"${THEIR_WIREGUARD_ENDPOINT}"'"'";"
+    fi
+
     VPN_CONFIG=$(
         cat <<END_HEREDOC
       ${PEERING_NAME} = {
-        peerLinkLocal = "fe80::cafe";
-        ownLinkLocal = "fe80::beef";
-        asNumber = "4242420611";
-        publicWireguardEndpoint = "pioneer.sebastians.dev:51822";
-        publicWireguardKey = "saICY1kV8JbuPOQNQLtm9TnVP2CuxC0qFSkd69pEKQQ=";
-        publicWireguardPort = "2";
+        peerLinkLocal = "${MY_LINK_LOCAL}";
+        ownLinkLocal = "${THEIR_LINK_LOCAL}";
+        asNumber = "${THEIR_AS}";
+        ${THEIR_WIREGUARD_ENDPOINT_LINE}
+        publicWireguardKey = "${THEIR_WIREGUARD_PUBLIC_KEY}";
+        publicWireguardPort = "${WIREGUARD_PORT}";
         # My public key: ${PUBLIC_KEY}
+        # My endpoint: ${MY_WIREGUARD_HOST}:${MY_WIREGUARD_PORT}
+        # My link local: ${MY_LINK_LOCAL}
+        # My AS: ${MY_AS}
       };
 END_HEREDOC
     )
@@ -93,10 +100,64 @@ if [ -n "$(git status --porcelain)" ]; then
     exit 1
 fi
 
-generateKeys
+MY_WIREGUARD_PORT="$(shuf -i 1-30000 -n 1)"
+MY_WIREGUARD_HOST="${PEERING_NAME}.dn42.antibuild.ing"
+MY_LINK_LOCAL="fe80::1234:9320"
+MY_AS="4242421403"
 
+echo "What is their wireguard host:port? (Leave empty if they dont have a static address)"
+read THEIR_WIREGUARD_ENDPOINT
+if [ -z "$THEIR_WIREGUARD_ENDPOINT" ]; then
+    echo "No wireguard endpoint given. Assuming they dont have a static address."
+fi
+
+echo "What is their wireguard public key? (Leave empty if you dont know yet)"
+read THEIR_WIREGUARD_PUBLIC_KEY
+if [ -z "$THEIR_WIREGUARD_PUBLIC_KEY" ]; then
+    THEIR_WIREGUARD_PUBLIC_KEY="TODO"
+fi
+
+echo "What is their link local address in the tunnel? (Leave empty if you dont know yet)"
+read THEIR_LINK_LOCAL
+if [ -z "$THEIR_LINK_LOCAL" ]; then
+    THEIR_LINK_LOCAL="fe80::8943:2034:2342"
+fi
+
+echo "What is their AS? (Leave empty if you dont know yet)"
+read THEIR_AS
+if [ -z "$THEIR_AS" ]; then
+    THEIR_AS="12345"
+fi
+
+generateKeys
 generateConfig
 
 echo "Finished generating config for $PEERING_NAME"
+echo "You can give them the following message:"
+echo ""
+echo "Hey there :) Here are the details for our new dn42 peering:"
+echo "My wireguard peer is reachable at \`${MY_WIREGUARD_HOST}:${MY_WIREGUARD_PORT}\`"
+echo "It uses the public key \`${PUBLIC_KEY}\`"
+if [ "$THEIR_WIREGUARD_PUBLIC_KEY" == "TODO" ]; then
+    echo "!! Please provide me with your wireguard public key !!"
+else
+    echo "Your public key is: \`${THEIR_WIREGUARD_PUBLIC_KEY}\`"
+fi
+if [ "$THEIR_WIREGUARD_ENDPOINT" != "" ]; then
+    echo "You have provided me with the endpoint \`${THEIR_WIREGUARD_ENDPOINT}\`"
+fi
+echo "Inside the tunnel, my link local address is \`${MY_LINK_LOCAL}\`\n"
+echo "Your link-local in the tunnel is \`${THEIR_LINK_LOCAL}\`\n"
+if [ "$THEIR_LINK_LOCAL" = "fe80::8943:2034:2342" ]; then
+    echo "!! If you want to use a different link-local address, please let me know !!"
+fi
+echo "My AS is \`${MY_AS}\`"
+if [ "$THEIR_AS" = "12345" ]; then
+    echo "!! I still need to know your AS number !!"
+else
+    echo "Your AS is \`${THEIR_AS}\`"
+fi
+
+echo "My public key"
 
 # ] <-- needed because of Argbash
