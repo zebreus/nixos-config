423bbd1b06942c2027d8fc8e6d09034c1f37b021
Zebreus <zebreus@zebre.us>
2025-01-18 12:38:38 +0100

    Start implementing easier dn42 peerings

--- modules/dn42/echonet_dn42.nix
+++ modules/dn42/echonet_dn42.nix
@@ -1,54 +1,62 @@
 # Establishes wireguard tunnels with all nodes with static IPs as hubs.
 { config, lib, ... }:
 let
-  cfg = config.machines.${config.networking.hostName}."${networkName}Dn42";
+  cfg = config.machines.${config.networking.hostName}."${peering.networkName}Dn42";
 
-  peerLinkLocal = "fe80::715";
-  ownLinkLocal = "fe80::1234:5678";
-  asNumber = "4242420714";
-  networkName = "echonet";
-  publicWireguardEndpoint = "de01.dn42.lare.cc:21403";
-  publicWireguardKey = "UrANRZ2S0WzUbo3cCIbT0PP/VAucFFeGLNP3aY/ovyM=";
-  publicWireguardPort = 5;
+  peering = {
+    peerLinkLocal = "fe80::715";
+    ownLinkLocal = "fe80::1234:5678";
+    asNumber = "4242420714";
+    networkName = "echonet";
+    publicWireguardEndpoint = "de01.dn42.lare.cc:21403";
+    publicWireguardKey = "UrANRZ2S0WzUbo3cCIbT0PP/VAucFFeGLNP3aY/ovyM=";
+    publicWireguardPort = 5;
+  };
 in
 {
   config = lib.mkIf cfg.enable {
-    age.secrets."${networkName}" = {
-      file = ../../secrets/${networkName}_dn42.age;
+    age.secrets."${peering.networkName}" = {
+      file = ../../secrets/${peering.networkName}_dn42.age;
       owner = "systemd-network";
       group = "systemd-network";
     };
 
     networking = {
-      firewall.allowedUDPPorts = [ publicWireguardPort ];
-      firewall.interfaces."${networkName}".allowedTCPPorts = [ 179 ];
+      firewall.allowedUDPPorts = [ peering.publicWireguardPort ];
+      firewall.interfaces."${peering.networkName}".allowedTCPPorts = [ 179 ];
     };
 
     systemd.network = {
       netdevs = {
-        "50-${networkName}" = {
+        "50-${peering.networkName}" = {
           netdevConfig = {
             Kind = "wireguard";
-            Name = "${networkName}";
+            Name = "${peering.networkName}";
             MTUBytes = "1420";
           };
           wireguardConfig = {
-            PrivateKeyFile = config.age.secrets."${networkName}".path;
-            ListenPort = publicWireguardPort;
+            PrivateKeyFile = config.age.secrets."${peering.networkName}".path;
+            ListenPort = peering.publicWireguardPort;
           };
-          wireguardPeers = [{
-            PublicKey = publicWireguardKey;
-            AllowedIPs = [ "::/0" "0.0.0.0/0" ];
-            Endpoint = publicWireguardEndpoint;
-            PersistentKeepalive = 25;
-          }];
+          wireguardPeers = [
+            ({
+              PublicKey = peering.publicWireguardKey;
+              AllowedIPs = [ "::/0" "0.0.0.0/0" ];
+              Endpoint = peering.publicWireguardEndpoint;
+              PersistentKeepalive = 25;
+            } ++ (
+              lib.mkIf (peering ? "publicWireguardEndpoint") {
+                Endpoint = peering.publicWireguardEndpoint;
+              }
+            ))
+          ];
         };
       };
-      networks."${networkName}" = {
-        matchConfig.Name = "${networkName}";
-        address = [ "${ownLinkLocal}/64" ];
+      networks."${peering.networkName}" = {
+        matchConfig.Name = "${peering.networkName}";
+        address = [ "${peering.ownLinkLocal}/64" ];
         routes = [{
-          Destination = "${peerLinkLocal}/128";
+          Destination = "${peering.peerLinkLocal}/128";
           Scope = "link";
         }];
         networkConfig = {
@@ -60,8 +68,8 @@ in
 
     services.bird2 = {
       config = lib.mkAfter ''
-        protocol bgp ${networkName} from dnpeers {
-            neighbor ${peerLinkLocal}%${networkName} as ${asNumber};
+        protocol bgp ${peering.networkName} from dnpeers {
+            neighbor ${peering.peerLinkLocal}%${peering.networkName} as ${peering.asNumber};
         }
       '';
     };
