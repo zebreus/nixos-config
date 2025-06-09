1a0b131b498a53370fdc7214c6e0172673b866b0
Zebreus <zebreus@zebre.us>
2025-06-09 15:45:47 +0200

    Fix locales

--- modules/common/locales.nix
+++ modules/common/locales.nix
@@ -4,22 +4,22 @@
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LANG = "en_US.utf8";
-    LANGUAGE = "en_US.utf8";
-    LC_ADDRESS = "en_US.utf8";
+    LANG = "en_US.UTF-8";
+    LANGUAGE = "en_US.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
     LC_COLLATE = "C.utf8";
-    LC_CTYPE = "en_US.utf8";
-    LC_IDENTIFICATION = "en_US.utf8";
-    LC_MEASUREMENT = "de_DE.utf8";
-    LC_MESSAGES = "en_US.utf8";
-    LC_MONETARY = "de_DE.utf8";
-    LC_NAME = "en_US.utf8";
-    LC_NUMERIC = "de_DE.utf8";
-    LC_PAPER = "de_DE.utf8";
-    LC_TELEPHONE = "de_DE.utf8";
-    LC_TIME = "de_DE.utf8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
   };
 
   # Configure keymap in X11
