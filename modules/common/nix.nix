0e0e3d8699e8ec3ac683e36d96b0fb452e3b425c
Zebreus <zebreus@zebre.us>
2026-08-02 22:54:13 +0200

    Just testing

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -2,6 +2,14 @@
 {
   nix = {
     package = pkgs.nixVersions.latest;
+    # When free space drops below min-free during a build, nix garbage
+    # collects until max-free is available instead of running the disk to 0
+    # and failing the build (which took down kashenblade and blanderdash on
+    # 2026-08-02).
+    settings = {
+      min-free = 3 * 1024 * 1024 * 1024;
+      max-free = 8 * 1024 * 1024 * 1024;
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
       extra-trusted-users = root lennart
